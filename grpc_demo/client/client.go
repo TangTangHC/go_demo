a62bfa57fbae141766226fc57cda186e55d34296
haochentang <haochentang@foxmail.com>
2022-11-13 17:37:31 +0800

    grpc metadata

--- grpc_demo/client/client.go
+++ grpc_demo/client/client.go
@@ -5,19 +5,42 @@ import (
 	"fmt"
 	"go-demo/grpc_demo/proto"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/metadata"
+	"time"
 )
 
 func main() {
+	interceptor := func(ctx context.Context, method string, req, reply interface{}, cc *grpc.ClientConn, invoker grpc.UnaryInvoker, opts ...grpc.CallOption) error {
+		start := time.Now()
+		err := invoker(ctx, method, req, reply, cc, opts...)
+		fmt.Printf("消耗时间：%s\n", time.Since(start))
+		return err
+	}
+	opt := grpc.WithUnaryInterceptor(interceptor)
+	//var opts []grpc.DialOption
+	opts := make([]grpc.DialOption, 2)
+	opts[0] = grpc.WithTransportCredentials(insecure.NewCredentials())
+	opts[1] = opt
+	//opts = append(opts, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	//opts = append(opts, opt)
 	// 推荐
-	//dial, err := grpc.Dial(":8080", grpc.WithTransportCredentials(insecure.NewCredentials()))
-	dial, err := grpc.Dial(":8080", grpc.WithInsecure())
+	dial, err := grpc.Dial(":8080", opts...)
+	//dial, err := grpc.Dial(":8080", grpc.WithTransportCredentials(insecure.NewCredentials()), opt)
+	//dial, err := grpc.Dial(":8080", grpc.WithInsecure())
 	if err != nil {
 
 	}
 	defer dial.Close()
 
 	client := proto.NewGreeterClient(dial)
-	hello, err := client.SayHello(context.Background(), &proto.HelloRequest{Name: "一个人"})
+	md := metadata.New(map[string]string{
+		"name":     "one person",
+		"password": "any pass",
+	})
+	ctx := metadata.NewOutgoingContext(context.Background(), md)
+
+	hello, err := client.SayHello(ctx, &proto.HelloRequest{Name: "一个人"})
 	if err != nil {
 		panic(err.Error())
 	}
--- grpc_demo/server/server.go
+++ grpc_demo/server/server.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/metadata"
 	"net"
 
 	"go-demo/grpc_demo/proto"
@@ -13,13 +15,30 @@ type Server struct {
 }
 
 func (s Server) SayHello(ctx context.Context, request *proto.HelloRequest) (*proto.HelloReply, error) {
+	md, ok := metadata.FromIncomingContext(ctx)
+	if !ok {
+		fmt.Println("get metadata error")
+	}
+	if name, ok := md["name"]; ok {
+		for i, v := range name {
+			fmt.Println(i, v)
+		}
+	}
 	return &proto.HelloReply{
 		Message: "Hello, " + request.Name,
 	}, nil
 }
 
 func main() {
-	g := grpc.NewServer()
+
+	inteceptor := func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (resp interface{}, err error) {
+		fmt.Println("接收到了一个新的请求")
+		i, err := handler(ctx, req)
+		fmt.Println("返回数据：", i)
+		return i, err
+	}
+	opt := grpc.UnaryInterceptor(inteceptor)
+	g := grpc.NewServer(opt)
 	proto.RegisterGreeterServer(g, &Server{})
 	listen, err := net.Listen("tcp", ":8080")
 	if err != nil {
