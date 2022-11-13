2c825a466a65a95d782a9ec5fc5ba2be23b1cc3f
haochentang <haochentang@foxmail.com>
2022-11-13 11:15:11 +0800

    protobuf demo

--- proto_demo/client/client.go
+++ proto_demo/client/client.go
@@ -1,5 +1,46 @@
 package main
 
+import (
+	"context"
+	"fmt"
+	"github.com/golang/protobuf/ptypes/empty"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/protobuf/types/known/timestamppb"
+	"time"
+
+	"go-demo/proto_demo/proto"
+)
+
 func main() {
+	dial, err := grpc.Dial(":9090", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		panic("连接失败" + err.Error())
+	}
+	defer dial.Close()
+
+	s := proto.HelloReply_Result{Name: "hh", Url: "www"}
+	fmt.Println(s)
+
+	h := proto.HelloReply{Sex: proto.Gender_FEMALE}
+	fmt.Println(h.String())
+
+	p := proto.HelloRequest{Map: map[string]string{
+		"k1": "v1",
+		"k2": "v2",
+	}, AddTime: timestamppb.New(time.Now())}
+	fmt.Println(p.String())
+
+	client := proto.NewGreeterClient(dial)
+	pong, _ := client.Ping(context.Background(), &empty.Empty{})
+	//pong, _ = client.Ping(context.Background(), &emptypb.Empty{})
+	fmt.Println(pong)
+	reply, err := client.SayHello(context.Background(), &proto.HelloRequest{
+		Name: "一个人",
+	})
+	if err != nil {
+		fmt.Println("调用失败", err.Error())
+	}
+	fmt.Println(reply.Msg)
 
 }
--- proto_demo/server/server.go
+++ proto_demo/server/server.go
@@ -1,5 +1,46 @@
 package main
 
-func main() {
+import (
+	"context"
+	"fmt"
+	"github.com/golang/protobuf/ptypes/empty"
+	"google.golang.org/grpc"
+	"net"
 
+	"go-demo/proto_demo/proto"
+)
+
+type server struct {
+	*proto.UnimplementedGreeterServer
+}
+
+func (s server) SayHello(context context.Context, req *proto.HelloRequest) (*proto.HelloReply, error) {
+	fmt.Println(req.Id)
+	fmt.Println(req.Name)
+	fmt.Println(req.IsDelete)
+	fmt.Println(req.Score)
+	fmt.Println(req.Titles)
+	fmt.Println(req.File)
+	return &proto.HelloReply{
+		Msg: "hello, " + req.Name,
+	}, nil
+}
+
+func (s server) Ping(context.Context, *empty.Empty) (*proto.Pong, error) {
+	return &proto.Pong{
+		Id: "success",
+	}, nil
+}
+
+func main() {
+	s := grpc.NewServer()
+	proto.RegisterGreeterServer(s, server{})
+	listen, err := net.Listen("tcp", ":9090")
+	if err != nil {
+		panic("listen error " + err.Error())
+	}
+	err = s.Serve(listen)
+	if err != nil {
+		panic("start error " + err.Error())
+	}
 }
